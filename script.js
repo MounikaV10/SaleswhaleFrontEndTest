c0b3fdcb3ef69552d03bf3de881a50f6f365d57a
MounikaV10 <56734751+MounikaV10@users.noreply.github.com>
2020-02-25 10:54:21 +0800

    updated script
    removed tab option and data rendering is done in div

---
+++ script.js
@@ -0,0 +1,273 @@
+var data = {
+    "teams": [
+      {
+        "id": 1,
+        "name": "Axa",
+        "image": "https://d1bb37ap2qun5z.cloudfront.net/shows/show_stub_avatars/000/000/966/twitter/wataten-avatar.jpg?1546777130",
+        "description": "AXA is a French multinational insurance firm headquartered in the 8th random place and random time etc",
+        "campaigns_count": 20,
+        "leads_count": 1500,
+        "is_favorited": true,
+        "is_archived": false,
+        "created_at": "28 July 2018"
+      },
+      {
+        "id": 2,
+        "name": "Indeed - US",
+        "image": "https://d1bb37ap2qun5z.cloudfront.net/shows/show_stub_avatars/000/000/970/twitter/shield-hero-avatar.jpg?1546779870",
+        "description": "Indeed is an American worldwide employment-related search engine for job stuff and other random stuff",
+        "campaigns_count": 15,
+        "leads_count": 7130,
+        "is_favorited": false,
+        "is_archived": false,
+        "created_at": "15 October 2018"
+      },
+      {
+        "id": 3,
+        "name": "Indeed - EU",
+        "image": "https://d1bb37ap2qun5z.cloudfront.net/shows/show_stub_avatars/000/000/970/twitter/shield-hero-avatar.jpg?1546779870",
+        "description": "Indeed is an American worldwide employment-related search engine for job stuff and other random stuff",
+        "campaigns_count": 18,
+        "leads_count": 3500,
+        "is_favorited": false,
+        "is_archived": false,
+        "created_at": "1 October 2018"
+      },
+      {
+        "id": 4,
+        "name": "Workday - US",
+        "image": "https://d1bb37ap2qun5z.cloudfront.net/shows/show_stub_avatars/000/000/908/twitter/tensura-avatar.png?1538236957",
+        "description": "Workday Inc is an on demand financial management and human capital management something とかなんとか",
+        "campaigns_count": 16,
+        "leads_count": 2000,
+        "is_favorited": false,
+        "is_archived": false
+      },
+      
+      {
+        "id": 5,
+        "name": "Workday - AU",
+        "image": "https://d1bb37ap2qun5z.cloudfront.net/shows/show_stub_avatars/000/000/908/twitter/tensura-avatar.png?1538236957",
+        "description": "Workday Inc is an on demand financial management and human capital management something とかなんとか",
+        "campaigns_count": 12,
+        "leads_count": 1500,
+        "is_favorited": false,
+        "is_archived": false
+      },
+      {
+        "id": 6,
+        "name": "Time",
+        "image": "https://d1bb37ap2qun5z.cloudfront.net/shows/show_stub_avatars/000/000/979/twitter/5-hanayome-avatar.jpg?1547281143",
+        "description": "Time is an American weekly news magazine and new website published in New York and like a bunch of other places and stuff",
+        "campaigns_count": 13,
+        "leads_count": 10000,
+        "is_favorited": true,
+        "is_archived": false
+      },
+      {
+        "id": 7,
+        "name": "Zendesk",
+        "image": "https://d1bb37ap2qun5z.cloudfront.net/shows/show_stub_avatars/000/000/924/twitter/uzamaid-avatar.png?1538475352",
+        "description": "Zendesk Inc is a customer service software company headquartered in San Francisco and they do things I guess",
+        "campaigns_count": 17,
+        "leads_count": 1200,
+        "is_favorited": true,
+        "is_archived": false
+      },
+      {
+        "id": 8,
+        "name": "Uber",
+        "image": "https://d1bb37ap2qun5z.cloudfront.net/shows/show_stub_avatars/000/000/965/twitter/kemono-friends-2-avatar.jpg?1546501240",
+        "description": "Uber is a transportation network company headquartered in San Francisco, California. They drive people around I guess",
+        "campaigns_count": 10,
+        "leads_count": 3200,
+        "is_favorited": false,
+        "is_archived": true
+      },
+      {
+        "id": 9,
+        "name": "Grab",
+        "image": "https://d1bb37ap2qun5z.cloudfront.net/shows/show_stub_avatars/000/000/895/twitter/revue-starlight-a.png?1531195655",
+        "description": "GrabTaxi Holdings Pte Ltd is a Singapore-based technology company that offers rides and other stuff but mostly rides to people",
+        "campaigns_count": 23,
+        "leads_count": 2000,
+        "is_favorited": false,
+        "is_archived": true
+      }
+    ],
+    "activities": [
+      {
+        "id": 1,
+        "person": {
+          "id": 1,
+          "name": "Julie",
+          "avatar": "https://d1bb37ap2qun5z.cloudfront.net/profiles/profile_avatars/000/000/003/display/tamako200x200b.png?1393742139"
+        },
+        "action": "increased_quota",
+        "target": "Indeed - US",
+        "created_at": "2 hours ago"
+      },
+      {
+        "id": 2,
+        "person": {
+          "id": 2,
+          "name": "Emily",
+          "avatar": "https://d1bb37ap2qun5z.cloudfront.net/sites/default_user_avatars/000/000/002/display/nicdesu.jpg?1394032656"
+        },
+        "action": "added_leads",
+        "target": "Linkedin",
+        "created_at": "4 hours ago"
+      },
+      {
+        "id": 3,
+        "person": {
+          "id": 2,
+          "name": "Emily",
+          "avatar": "https://d1bb37ap2qun5z.cloudfront.net/sites/default_user_avatars/000/000/002/display/nicdesu.jpg?1394032656"
+        },
+        "action": "archived_team",
+        "target": "Uber"
+      },
+      {
+        "id": 4,
+        "person": {
+          "id": 1,
+          "name": "Julie",
+          "avatar": "https://d1bb37ap2qun5z.cloudfront.net/profiles/profile_avatars/000/000/003/display/tamako200x200b.png?1393742139"
+        },
+        "action": "archived_team",
+        "target": "Grab",
+        "created_at": "6 hours ago"
+      },
+      {
+        "id": 5,
+        "person": {
+          "id": 1,
+          "name": "Julie",
+          "avatar": "https://d1bb37ap2qun5z.cloudfront.net/profiles/profile_avatars/000/000/003/display/tamako200x200b.png?1393742139"
+        },
+        "action": "added_leads",
+        "target": "Workday - AU",
+        "created_at": "2 hours ago"
+      }
+    ],
+    "current_user": {
+      "id": 4,
+      "name": "John",
+      "avatar": "https://d1bb37ap2qun5z.cloudfront.net/profiles/profile_avatars/000/000/001/display/shimakaze-desu.jpg?1396709040",
+      "notifications_count": 5
+    }
+  }
+
+var teams = data.teams;
+var lenTeam = teams.length;
+
+function printTeams(){
+    var htmlOutput = '<div class="row teamfetch">';
+    for(i=0;i<lenTeam;i++){
+      htmlOutput += '<div class="col-sm-4 col-lg-4">'
+        +'<div class="card teams"><div class="card-body">';
+      if(teams[i].is_favorited==true){
+        htmlOutput += '<i class="fa fa-star checked float-right"></i>';
+      }else{
+        htmlOutput += '<i class="fa fa-star-o float-right"></i>';
+      }
+      htmlOutput += '<div class="message"><img src="'+teams[i].image+'" class="team-card-avatar float-left rounded"><h6 class="card-title">'
+      +teams[i].name+'</h6><p class="card-time">Created on'
+      +teams[i].created_at+'</p></div><div class="team-desc"><p class="card-text ellipsis">'
+      +teams[i].description+'</p></div></div><div class="card-footer"><i class="fa fa-comments-o float-left ">'
+      +teams[i].campaigns_count+'Campaigns</i><i class="fa fa-address-card-o float-right ">'
+      +teams[i].leads_count+'Leads</i></div></div></div>';
+  }
+  htmlOutput +='</div>';
+  $('#datafetch').html('').html(htmlOutput);
+}
+
+function printFavorites(){
+    var htmlOutput = '<div class="row teamfetch">'; 
+    for(i=0;i<lenTeam;i++){
+        if(teams[i].is_favorited==true){
+            htmlOutput += '<div class="col-sm-4 col-lg-4"><div class="card teams"><div class="card-body"><i class="fa fa-star checked float-right"></i><div class="message"><img src="'
+            +teams[i].image+'" class="team-card-avatar float-left rounded"><h6 class="card-title">'
+            +teams[i].name+'</h6><p class="card-time">Created on'
+            +teams[i].created_at+'</p></div><div class="team-desc"><p class="card-text ellipsis">'
+            +teams[i].description+'</p></div></div><div class="card-footer"><i class="fa fa-comments-o float-left ">'
+            +teams[i].campaigns_count+'Campaigns</i><i class="fa fa-address-card-o float-right ">'
+            +teams[i].leads_count+'Leads</i></div></div></div>';
+
+        }
+    }
+    htmlOutput +='</div>';
+    $('#datafetch').html('').html(htmlOutput);
+}
+
+function printArchived(){
+    var htmlOutput = '<div class="row teamfetch">'; 
+    for(i=0;i<lenTeam;i++){
+        if(teams[i].is_archived==true){
+          htmlOutput += '<div class="col-sm-4 col-lg-4">'
+          +'<div class="card teams"><div class="card-body">';
+        if(teams[i].is_favorited==true){
+          htmlOutput += '<i class="fa fa-star checked float-right"></i>';
+        }else{
+          htmlOutput += '<i class="fa fa-star-o float-right"></i>';
+        }
+        htmlOutput += '<div class="message"><img src="'+teams[i].image+'" class="team-card-avatar float-left rounded"><h6 class="card-title">'
+        +teams[i].name+'</h6><p class="card-time">Created on'
+        +teams[i].created_at+'</p></div><div class="team-desc"><p class="card-text ellipsis">'
+        +teams[i].description+'</p></div></div><div class="card-footer"><i class="fa fa-comments-o float-left ">'
+        +teams[i].campaigns_count+'Campaigns</i><i class="fa fa-address-card-o float-right ">'
+        +teams[i].leads_count+'Leads</i></div></div></div>';
+        }
+    }
+    htmlOutput +='</div>';
+    $('#datafetch').html('').html(htmlOutput);
+}
+
+function printActivity(){
+    var activities = data.activities;
+    lenActivity = activities.length;
+    var htmlActivityOutput = '<div class="row">';
+    for(i=0;i<lenActivity;i++){
+        htmlActivityOutput += '<div class="user-activity"><img src="'
+        +activities[i]["person"]["avatar"]
+        +'"class="team-card-avatar rounded-circle float-left"><div class="activity-data"><p><strong>'
+        +activities[i]["person"]["name"]+'</strong>&nbsp'
+        +activities[i].action.replace("_",'&nbsp<strong>'+activities[i].target+'</strong>&nbsp')+'</p><p>'
+        +activities[i] .created_at+'</p></div></div>';
+    }
+    htmlActivityOutput +='</div>';
+    $('#activity-card').html('').html(htmlActivityOutput);
+}
+
+function printCurrentUser(){
+    var current_user = data.current_user;
+    htmlUserImage = '<img src="'+current_user.avatar+'" class="user-avatar rounded-circle">';
+    htmlUserName = '<h6>Hello,'+current_user.name+'</h6>';
+    htmlNotifications = current_user.notifications_count;
+    $('#current-user-name').html('').html(htmlUserName);
+    $('#current-user-image').html('').html(htmlUserImage);
+    $('#notification-count').html('').html(htmlNotifications);    
+    printActivity();  
+}
+
+
+$(document).ready(function(){
+  printCurrentUser();
+  printActivity();
+  printTeams();
+
+  $('#btnfav').change(function(){
+    //   alert("Favorites!");
+    printFavorites();
+  });
+  $('#btnarch').change(function(){
+    // alert("Archived tab");
+    printArchived();
+  });
+  $('#btnall').change(function(){
+    // alert("All tab");
+    printTeams();
+  });
+
+});
